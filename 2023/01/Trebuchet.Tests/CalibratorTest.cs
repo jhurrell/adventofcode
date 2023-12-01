9bf45794eb3cb15913a5f20432bef1b45d290695
John Hurrell <jhurrell@gmail.com>
2023-12-01 14:35:43 -0500

    Converted Fact to Theory

--- 2023/01/Trebuchet.Tests/CalibratorTest.cs
+++ 2023/01/Trebuchet.Tests/CalibratorTest.cs
@@ -4,11 +4,13 @@ public class CalibratorTests
 {
     [Theory]
 
+    // Part 1
     [InlineData(12, "1abc2")]
     [InlineData(38, "pqr3stu8vwx")]
     [InlineData(15, "a1b2c3d4e5f")]
     [InlineData(77, "treb7uchet")]
 
+    // Part 2
     [InlineData(29, "two1nine")]
     [InlineData(83, "eightwothree")]
     [InlineData(13, "abcone2threexyz")]
@@ -17,38 +19,18 @@ public class CalibratorTests
     [InlineData(14, "zoneight234")]
     [InlineData(76, "7pqrstsixteen")]
 
+    // Gotchas...
     [InlineData(82, "eightwo")]
     public void DecodeLine(int expected, string input)
     {
         Assert.Equal(expected, Calibrator.DecodeLine(input));
     }
 
-    [Fact]
-    public void DecodeCalibrationValues_Part1()
+    [Theory]
+    [InlineData(142, new string[] { "1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet" })]
+    [InlineData(281, new string[] { "two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen" })]
+    public void DecodeCalibrationValues(int expected, IEnumerable<string> values)
     {
-        var examples = new List<string> {
-            "1abc2",
-            "pqr3stu8vwx",
-            "a1b2c3d4e5f",
-            "treb7uchet"
-        };
-
-        Assert.Equal(142, Calibrator.DecodeCalibrationValues(examples));
+        Assert.Equal(expected, Calibrator.DecodeCalibrationValues(values));
     }
-
-    [Fact]
-    public void DecodeCalibrationValues_Part2()
-    {
-        var examples = new List<string> {
-            "two1nine",
-            "eightwothree",
-            "abcone2threexyz",
-            "xtwone3four",
-            "4nineeightseven2",
-            "zoneight234",
-            "7pqrstsixteen",
-        };
-
-        Assert.Equal(281, Calibrator.DecodeCalibrationValues(examples));
-    }    
 }
